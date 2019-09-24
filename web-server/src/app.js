2077abfc81f04b41ed6c11632a439a7acfa889f8
Shobhu20 <shobhit.2094@gmail.com>
2019-09-23 21:36:40 -0400

    changed photo

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -20,14 +20,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'Shobhit Saxena'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Shobhit Saxena'
     })
 })
 
@@ -35,7 +35,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Andrew Mead'
+        name: 'Shobhit Saxena'
     })
 })
 
@@ -69,7 +69,7 @@ app.get('/products', (req, res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Shobhit Saxena',
         errorMessage: 'Help article not found.'
     })
 })
@@ -77,7 +77,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Shobhit Saxena',
         errorMessage: 'Page not found.'
     })
 })
